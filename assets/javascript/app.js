c36fa52283021ef23760c4fb9b58c60f64504184
mclinch <michael.clinch@gmail.com>
2017-05-27 12:29:16 -0700

    commit minus alert

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,4 @@
-
-
-
-
-                  
+                 
 // ------------------------------------------------------------------------------------
 $(document).ready(function(){
 //FIREBASE=========================================================
@@ -21,6 +17,11 @@ var database = firebase.database();
 var currentTime = moment();
 console.log("Current Time: " + currentTime);
 //FUNCTIONS=========================================================
+
+  $("#name-input").val("");
+  $("#dest-input").val("");
+  $("#time-input").val("");
+  $("#freq-input").val("");
 
 // CAPTURE BUTTON CLICK
 $("#add-train").on("click", function(event) {
@@ -50,11 +51,17 @@ $("#add-train").on("click", function(event) {
   console.log(newTrain.dest);
   console.log(newTrain.time);
   console.log(newTrain.freq);
-  alert("new train added")
+  // alert("new train added")
       // });
      // NO REFRESH
-     $("input").val('');
-    return false;
+  $("#name-input").val("");
+  $("#dest-input").val("");
+  $("#time-input").val("");
+  $("#freq-input").val("");
+
+
+    //  $("input").val('');
+    // return false;
 });
 
 //ON CLICK CHILD FUNCTION
@@ -131,93 +138,3 @@ $("body").on("click", ".remove-train", function(){
 }); //END DOCUMENT.READY
 
 
-
-
-
-// -----------------------------------------------------------------------------------
-
-// var url ="https://trainscheduler-52919.firebaseio.com";
-// var dataRef = firebase.database().ref().child('train');
-// var name ='';
-// var destination = '';
-// var firstTrainTime = '';
-// var frequency = '';
-// var nextTrain = '';
-// var nextTrainFormatted = '';
-// var minutesAway = '';
-// var firstTimeConverted = '';
-// var currentTime = '';
-// var diffTime = '';
-// var tRemainder = '';
-// var minutesTillTrain = '';
-// var keyHolder = '';
-// var getKey = '';
-
-
-// $(document).ready(function() {
-
-//      $("#add-train").on("click", function() {
-
-//      	name = $('#name-input').val().trim();
-//      	destination = $('#destination-input').val().trim();
-//      	firstTrainTime = $('#first-train-time-input').val().trim();
-//      	frequency = $('#frequency-input').val().trim();
-//           firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
-//           currentTime = moment();
-//           diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-//           tRemainder = diffTime % frequency;
-//           minutesTillTrain = frequency - tRemainder;
-//           nextTrain = moment().add(minutesTillTrain, "minutes");
-//           nextTrainFormatted = moment(nextTrain).format("hh:mm");
-
-//      	// Code for the push
-//      	keyHolder = dataRef.push({
-//      		name: name,
-//      		destination: destination,
-//      		firstTrainTime: firstTrainTime,  // 2:22 in my example
-//      		frequency: frequency,
-//                nextTrainFormatted: nextTrainFormatted,
-//                minutesTillTrain: minutesTillTrain
-//      	});
-//           // The notes below are for finding the path to the key in the data being pushed, leaving as notes to save for later use.
-//           console.log(keyHolder.path.u[0]);
-//           var key = keyHolder.path.u[0];
-//           console.log(key);
-//      	// Don't refresh the page!
-
-//           $('#name-input').val('');
-//      	$('#destination-input').val('');
-//      	$('#first-train-time-input').val('');
-//      	$('#frequency-input').val('');
-
-//      	return false;
-//      });
-//           //id=" + "'" + keyHolder.path.u[0] + "'" + "
-//      dataRef.on("child_added", function(childSnapshot) {
-// 	// full list of items to the well
-
-// 		$('.train-schedule').append("<tr class='table-row' id=" + "'" + childSnapshot.key() + "'" + ">" +
-//                "<td class='col-xs-3'>" + childSnapshot.val().name +
-//                "</td>" +
-//                "<td class='col-xs-2'>" + childSnapshot.val().destination +
-//                "</td>" +
-//                "<td class='col-xs-2'>" + childSnapshot.val().frequency +
-//                "</td>" +
-//                "<td class='col-xs-2'>" + childSnapshot.val().nextTrainFormatted + // Next Arrival Formula ()
-//                "</td>" +
-//                "<td class='col-xs-2'>" + childSnapshot.val().minutesTillTrain + // Minutes Away Formula
-//                "</td>" +
-//                "<td class='col-xs-1'>" + "<input type='submit' value='remove train' class='remove-train btn btn-primary btn-sm'>" + "</td>" +
-//           "</tr>");
-// // Handle the errors
-// }, function(errorObject){
-// 	//console.log("Errors handled: " + errorObject.code)
-// });
-
-// $("body").on("click", ".remove-train", function(){
-//      $(this).closest ('tr').remove();
-//      getKey = $(this).parent().parent().attr('id');
-//      dataRef.child(getKey).remove();
-// });
-
-// }); // Closes jQuery wrapper
